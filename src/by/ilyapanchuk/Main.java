3372e7929e4bb923fa0365383a1d4f0caa78c619
IlyaChernikin <IlyaChernikin12@gmail.com>
2023-02-07 17:37:50 +0300

    Initial commit

---
+++ src/by/ilyapanchuk/Main.java
@@ -0,0 +1,9 @@
+package by.ilyapanchuk;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //TODO Hello, this is Ilya
+    }
+}
